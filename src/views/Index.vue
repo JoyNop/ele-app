318bfd85b957a3e4172ad850ae0a2232a51a1e18
HanRui <597589373@qq.com>
2019-07-17 14:44:30 +0800

    1

---
+++ src/views/Index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="index">
+    主页
+    <router-view></router-view>
+    <!-- <TabBar :data="tabbarData"/> -->
+  </div>
+</template>
+
+<script>
+// import TabBar from "../components/TabBar";
+export default {
+  name: "Index",
+  
+};
+</script>
+
+<style scoped>
+.index {
+  width: 100%;
+  height: calc(100% - 45px);
+}
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class='login'>
+    登录
+  </div>
+</template>
+
+<script>
+export default {
+  name:'login'
+}
+</script>
+
+
+<style scoped>
+.login {
+  width: 100%;
+  height: 100%;
+  padding: 30px;
+  box-sizing: border-box;
+  background: #fff;
+}
+
+.logo {
+  text-align: center;
+}
+.logo img {
+  width: 150px;
+}
+.text_group,
+.login_des,
+.login_btn {
+  margin-top: 20px;
+}
+.login_des {
+  color: #aaa;
+  line-height: 22px;
+}
+.login_des span {
+  color: #4d90fe;
+}
+.login_btn button {
+  width: 100%;
+  height: 40px;
+  background-color: #48ca38;
+  border-radius: 4px;
+  color: white;
+  font-size: 14px;
+  border: none;
+  outline: none;
+}
+.login_btn button[disabled] {
+  background-color: #8bda81;
+}
+</style>
